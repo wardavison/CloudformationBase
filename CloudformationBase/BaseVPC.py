004f891fa8a6a0f30416a217ed4067e0aee7a5e1
wardavison <wardavison@gmail.com>
2014-09-29 00:14:22 +0100

    Starting to break things out into parameterised methods

--- CloudformationBase/BaseVPC.py
+++ CloudformationBase/BaseVPC.py
@@ -13,41 +13,78 @@ class BaseVPC(object):
     def __init__(self, projectname, environmentname):
         self.projectname = projectname
         self.environmentname = environmentname
+        self.parameters = []
+        self.mappings = []
+        self.resources = []
+        self.outputs = []
+        self.basevpc = None
+        self.set_resources()
 
-    @property
-    @abc.abstractmethod
-    def res_vpc(self):
+    def res_vpc(self, title, cidrblock, dnssupport, dnshostnames,
+                instancetenancy):
+        """ Creates a Troposphere VPC resource object
+        The name is generated from the projectname and environment name
+        :param title: Title for the JSON VPC resource (string)
+        :param cidrblock: CIDR block for the VPC e.g. 10.0.0.0/16 (string)
+        :param dnssupport: The VPC has DNS support (boolean)
+        :param dnshostnames: Enable DNS hostnames (boolean)
+        :param instancetenancy: default or dedicated instance tenancy (string)
+        """
         return VPC(
-            'BaseVPC',
-            CidrBlock='10.0.0.0/16',
-            EnableDnsSupport=True,
-            EnableDnsHostnames=True,
-            InstanceTenancy='default',
+            title,
+            CidrBlock=cidrblock,
+            EnableDnsSupport=dnssupport,
+            EnableDnsHostnames=dnshostnames,
+            InstanceTenancy=instancetenancy,
             Tags=[Tag('Name', Join('', [self.projectname,
                                         '-',
                                         self.environmentname,
                                         '-VPC']))]
         )
 
-    #@abc.abstractmethod
-    #def set_parameters(self):
-    #    """ Define parameters for use by the VPC
-    #    """
-    #    return
+    def set_parameters(self):
+        pass
 
-    #@abc.abstractmethod
-    #def set_mappings(self):
-    #    """ Define mappings for use in the template
-    #    """
-    #    return
+    def set_mappings(self):
+        pass
 
-    #@abc.abstractmethod
-    #def set_resources(self):
-    #    """ Define resources that make up the VPC
-    #    """
-    #    return
+    def set_resources(self):
+        self.basevpc = self.res_vpc('BaseVPC', '10.0.0.0/16',
+                                    True, True, 'default')
+        self.resources.append(self.basevpc)
 
-    #@abc.abstractmethod
-    #def set_outputs(self):
-    #    """ Define outputs to generated from VPC resources
-    #    """
+    def set_outputs(self):
+        """
+        :return:
+        """
+        pass
+
+    @property
+    @abc.abstractmethod
+    def get_parameters(self):
+        """ Return parameters for use by the VPC
+        """
+        return self.parameters
+
+    @property
+    @abc.abstractmethod
+    def get_mappings(self):
+        """ Return mappings for use in the template
+        """
+        return self.mappings
+
+    @property
+    @abc.abstractmethod
+    def get_resources(self):
+        """ Return resources that make up the VPC
+        :return: list of Troposphere resource objects
+        :rtype : list
+        """
+        return self.resources
+
+    @property
+    @abc.abstractmethod
+    def get_outputs(self):
+        """ Return outputs generated from VPC resources
+        """
+        return self.outputs
